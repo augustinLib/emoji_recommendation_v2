de52d4e83eddfcf6625d0cc6227b09f133ea3489
2bambitious <2bambitious@gmail.com>
2022-08-02 01:14:41 +0900

    update loader with torchtext

--- emoji_classification/loader.py
+++ emoji_classification/loader.py
@@ -1,77 +1,72 @@
-import torch
-import pandas as pd
+from torchtext.legacy import data
 
-from torch.utils.data import Dataset, DataLoader
 
-class MyDataset(Dataset):
 
-    def __init__(self, data, labels):
-        self.data = data
-        self.labels = labels
+class DataLoader(object):
+
+    def __init__(
+        self, 
+        train_filename,
+        batch_size = 64,
+        vaild_ratio = .2,
+        device = -1,
+        # 전체 단어 maximum 개수
+        max_vocab = 999999,
+        # 최소 몇번 나온얘들 추가할건지
+        min_freq=1,
+        # end_of_sentence 사용 여부
+        use_eos = False,
+        shuffle = True):
 
         super().__init__()
-
-    
-    def __len__(self):
-        return self.data.size(0)
-
-
-    def __getitem__(self, index):
         
-        x = self.data[index]
-        y = self.labels[index]
+        #Field 정의
+        self.label = data.Field(
+            # class만 있기 때문에 non-sequential
+            sequential = False,
+            use_vocab = True,
+            # 모르는 class가 있으면 안됨
+            unk_token = False
+        )
 
-        return x, y
+        self.text = data.Field(
+            use_vocab = True,
+            batch_first = True,
+            include_lengths = False,
+            eos_token = '<EOS>' if use_eos else None
+        )
+
+        # Dataset
+        train, valid = data.TabularDataset(
+            # 파일 불러오기
+            path = train_filename,
+            format = 'tsv',
+            # 앞서 정의한 Field객체 추가
+            fields = [
+                ('label', self.label),
+                ('text', self.text),
+            ],
+        #train_ratio 지정
+        ).split(split_ratio=(1-vaild_ratio))
+
+        # DataLoader
+        self.train_loader, self.valid_loader = data.BucketIterator.splits(
+        # splits()을 이용해서 train_loader와 valid_loader를 각각 생성
+        (train, valid),
+        batch_size = batch_size,
+        device='cuda:%d' % device if device >= 0 else 'cpu',
+        shuffle=shuffle,
+        # 문장 길이로 batch 형성
+        sort_key=lambda x: len(x.text),
+        # mini batch내에서 sorting 할거냐 (batch 안에서도 길이에 따라서 정렬)
+        sort_within_batch=True,
+        )
+
+        # 단어사전 만들기
+        # label에 대한 vocabulary
+        self.label.build_vocab(train)
+        # text에 대한 vocabulary
+        self.text.build_vocab(train, max_size=max_vocab, min_freq=min_freq)
 
 
-def load_senitimental_data():
-    dataset = pd.read_csv("../data/sentimental.csv")
-
-    x = dataset["sentences"]
-    y = dataset["sentiment"]
-
-    return x, y
-
-
-def get_loaders(config):
-    x, y = load_senitimental_data()
-
-    train_cnt = int(x.size(0) * config.train_ratio) 
-    valid_cnt = int(x.size(0) * config.vaild_ratio) 
-    test_cnt = x.size(0) - train_cnt - valid_cnt
-
-    indices = torch.randperm(x.size(0))
-
-    train_x, valid_x, test_x = torch.index_select(
-        x, 
-        dim=0, 
-        index=indices
-    ).split([train_cnt, valid_cnt, test_cnt], dim = 0)
-
-
-    train_y, valid_y, test_y = torch.index_select(
-        y, 
-        dim=0, 
-        index=indices
-    ).split([train_cnt, valid_cnt, test_cnt], dim = 0)
-
-    train_loader = DataLoader(
-        dataset= MyDataset(train_x, train_y),
-        batch_size=config.batch_size,
-        shuffle=True,
-    )
-
-    valid_loader = DataLoader(
-        dataset= MyDataset(valid_x, valid_y),
-        batch_size=config.batch_size,
-        shuffle=True,
-    )
-
-    test_loader = DataLoader(
-        dataset= MyDataset(test_x, test_y),
-        batch_size=config.batch_size,
-        shuffle=False,
-    )
-
-    return train_loader, valid_loader, test_loader
 
