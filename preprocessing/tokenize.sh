1b20db6c39f812878abcb924a3d455d0cfb2fce4
2bambitious <2bambitious@gmail.com>
2022-08-03 17:51:01 +0900

    Fix about bpe

--- preprocessing/tokenize.sh
+++ preprocessing/tokenize.sh
@@ -2,8 +2,20 @@
 
 FILENAME=$1
 TARGET_FILENAME=$2
+MODEL_FILENAME=$3
+N_SYMBOLS=50000
 
+cut -f1 ${FILENAME} > ${FILENAME}.text
 cut -f2 ${FILENAME} > ${FILENAME}.label
-cut -f1 ${FILENAME} | mecab -O wakati > ${FILENAME}.text.tok
+
+cat ${FILENAME}.text | mecab -O wakati > ${FILENAME}.text.tok
+
+
+subword-nmt learn-bpe -s ${N_SYMBOLS} < ${FILENAME}.text.tok > ${MODEL_FILENAME}
+subword-nmt apply-bpe -c ${MODEL_FILENAME} < ${FILENAME}.text.tok > ${TARGET_FILENAME}.text.tok
+
+#./learn_bpe.py -s 30000 < ${FILENAME}.text.tok > ../emoji_classification/models/bpe_model
+#./apply_bpe.py -c ../../emoji_classification/models/bpe_model < ${FILENAME}.text.tok > ${TARGET_FILENAME}.text.tok
+
 
 paste ${FILENAME}.text.tok ${FILENAME}.label > ${TARGET_FILENAME}
