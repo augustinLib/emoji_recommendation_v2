3f8a8184016a1169742079c36f5bd1c0222e5a24
2bambitious <2bambitious@gmail.com>
2022-08-07 23:48:32 +0900

    Fix trainer

--- emoji_classification/trainer.py
+++ emoji_classification/trainer.py
@@ -156,6 +156,7 @@ class MyEngine(Engine):
 
     @staticmethod
     def save_model(engine, train_engine, config, **kwargs):
+
         torch.save(
             {
                 'model': engine.best_model,
@@ -163,6 +164,7 @@ class MyEngine(Engine):
                 **kwargs
             }, config.model_fn
         )
+
 
 
 class Trainer():
